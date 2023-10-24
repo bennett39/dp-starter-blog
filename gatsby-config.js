df35cd712cb50a82d7c9ea37c4d9f5f7ff37bc3a
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2023-10-24 08:40:17 -0400

    feat: update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Developer Purpose: Daily`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Bennett Garner`,
+      summary: `a staff engineer in Boston.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `I write something new for software developers every day.`,
+    siteUrl: `https://daily.developerpurpose.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `bennettgarner`,
     },
   },
   plugins: [
@@ -110,15 +110,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Developer Purpose: Daily`,
+        short_name: `Developer Purpose`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dp-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
